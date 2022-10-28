e116cea1940ece4f62b681a3922bf66616ed50fe
Oluwabunmi Fife <oluwabunmifife@gmail.com>
2022-10-28 14:28:09 +0100

    Modified views

--- main/views.py
+++ main/views.py
@@ -8,7 +8,22 @@ from .serializers import SlackPersonSerializer
 # Create your views here.
 @csrf_exempt
 def person_list(request):
+    header = {"Access-Control-Allow-Origin":"*"}
     if request.method == 'GET':
         people = SlackPerson.objects.all()
         serializer = SlackPersonSerializer(people, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return JsonResponse(serializer.data, safe=False, headers=header)
+
+
+# from rest_framework.decorators import api_view
+# from django.http import JsonResponse
+# @api_view(["GET"])
+# def home(request, *args, **kwargs):
+#   header = {"Access-Control-Allow-Origin":"*"}
+#   data = {
+#     "slackUsername":"",
+#     "backend":True,
+#     "age": yourage,
+#     "bio":"your bio"
+#   }
+#   return JsonResponse(data,headers=header)
